1f976a2e34e41efe46dd7db754bd5c24edba0a20
Peter <petershotbox@gmail.com>
2025-02-02 21:18:46 +0000

    Added app renders and mobile compatibilty

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const stickViewButton = document.getElementById('stickView');
+    const appViewButton = document.getElementById('appView');
+
+    function toggleButtons(selectedButton, otherButton) {
+        selectedButton.classList.add('selectedToggle');
+        selectedButton.classList.remove('defaultToggle');
+        otherButton.classList.add('defaultToggle');
+        otherButton.classList.remove('selectedToggle');
+        if (selectedButton.id === 'appView') {
+            document.getElementById('render2').classList.add('hidden');
+            document.getElementById('appRender').classList.remove('hidden');
+        } else if (selectedButton.id === 'stickView') {
+            document.getElementById('appRender').classList.add('hidden');
+            document.getElementById('render2').classList.remove('hidden');
+        }  }
+
+    stickViewButton.addEventListener('click', () => {
+        toggleButtons(stickViewButton, appViewButton);
+    });
+
+    appViewButton.addEventListener('click', () => {
+        toggleButtons(appViewButton, stickViewButton);
+    });
+});
