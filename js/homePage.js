0eb485defb7f9f3fe4412ca1af5ba0f0fe91b56c
Peter <petershotbox@gmail.com>
2025-02-09 22:08:54 +0000

    Added pre order section

--- js/homePage.js
+++ js/homePage.js
@@ -25,3 +25,55 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleButtons(appViewButton, stickViewButton);
     });
 });
+
+
+// When stick render clicked, the expand icon expands - encouraging user to click on it.
+const expandIcon = document.getElementById('expandIcon');
+const stick1 = document.getElementById('V1render');
+const stick2 = document.getElementById('V2render');
+const stick3 = document.getElementById('V3render');
+stick1.addEventListener('click', () => {
+    expandIcon.classList.add('highlight');
+    setTimeout(() => {
+        expandIcon.classList.remove('highlight');
+    }, 500);
+});
+stick2.addEventListener('click', () => {
+    expandIcon.classList.add('highlight');
+    setTimeout(() => {
+        expandIcon.classList.remove('highlight');
+    }, 500);
+});
+stick3.addEventListener('click', () => {
+    expandIcon.classList.add('highlight');
+    setTimeout(() => {
+        expandIcon.classList.remove('highlight');
+    }, 500);
+});
+
+
+// Handles and verifies email input.
+const emailButton = document.getElementById('emailButton');
+const emailInput = document.getElementById('emailInput');
+
+emailButton.addEventListener('click', async() => {
+    const emailText = emailInput.value;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailPattern.test(emailText)) {
+        let save = await saveEmail(emailText);
+        if(save){
+            emailButton.disabled = true;
+            emailInput.disabled = true;
+            document.getElementById('emailSuccess').style.display = 'block';
+            return
+        }
+        console.log('Invalid save');
+    }
+    console.log('Invalid email');
+});
+
+
+async function saveEmail(emailInput){
+    // TODO
+    return true;
+}
